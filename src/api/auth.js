8242b3a363bd4e55c7c8fc71afda3d808b26552f
mrbaummath <baum1286@gmail.com>
2022-11-27 20:37:10 -0500

    first commit with the auth boilerplate. Had to delete 'token=' from the auth header in axios calls

---
+++ src/api/auth.js
@@ -0,0 +1,56 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+
+
+export const signUp = (credentials) => {
+	return axios({
+		method: 'POST',
+		url: `apiUrl/accounts/signup`,
+		data: {
+			credentials: {
+				username: credentials.username,
+				password: credentials.password,
+				password_confirmation: credentials.passwordConfirmation,
+			},
+		},
+	})
+}
+
+export const signIn = (credentials) => {
+	return axios({
+		url: `${apiUrl}/accounts/login/`,
+		method: 'POST',
+		data: {
+			 	username: credentials.username,
+				password: credentials.password,
+			},
+		
+	})
+}
+
+export const signOut = (user) => {
+	return axios({
+		url: `${apiUrl}/accounts/logout/`,
+		method: 'DELETE',
+		headers: {
+			Authorization: `Token ${user.token}`,
+		},
+	})
+}
+
+export const changePassword = (passwords, user) => {
+	return axios({
+		url: apiUrl + '/change-password',
+		method: 'PATCH',
+		headers: {
+			Authorization: `Token ${user.token}`,
+		},
+		data: {
+			passwords: {
+				old: passwords.oldPassword,
+				new: passwords.newPassword,
+			},
+		},
+	})
+}
---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { signIn } from '../../api/auth'
+import messages from '../shared/AutoDismissAlert/messages'
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const SignIn = (props) => {
+	
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const navigate = useNavigate()
+
+	// handleChange = (event) =>
+	// 	this.setState({
+	// 		[event.target.name]: event.target.value,
+	// 	})
+
+	const onSignIn = (event) => {
+		event.preventDefault()
+		const { msgAlert, setUser } = props
+        const credentials = {username, password}
+		signIn(credentials)
+			.then((res) => setUser(res.data.user))
+			.then(() =>
+				msgAlert({
+					heading: 'Sign In Success',
+					message: messages.signInSuccess,
+					variant: 'success',
+				})
+			)
+			.then(() => navigate('/'))
+			.catch((error) => {
+                setUsername('')
+                setPassword('')
+				msgAlert({
+					heading: 'Sign In Failed with error: ' + error.message,
+					message: messages.signInFailure,
+					variant: 'danger',
+				})
+			})
+	}
+
+    return (
+        <div className='row'>
+            <div className='col-sm-10 col-md-8 mx-auto mt-5'>
+                <h3>Sign In</h3>
+                <Form onSubmit={onSignIn}>
+                    <Form.Group controlId='email'>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+                            required
+                            type='text'
+                            name='username'
+                            value={username}
+                            placeholder='Enter username'
+                            onChange={e => setUsername(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId='password'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            required
+                            name='password'
+                            value={password}
+                            type='password'
+                            placeholder='Password'
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Button variant='primary' type='submit'>
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn
